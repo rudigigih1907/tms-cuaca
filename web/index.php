9857254764b5b946eab6be6c77c7dfb69f305587
Rudi Gigih Prabowo <rudigigih0719@gmail.com>
2026-08-19 14:03:28 +0700

    chore: install vlucas/phpdotenv and setup in yii2 - Add vlucas/phpdotenv to composer.json - Initialize Dotenv in web/index.php entry script - Create .env.example template file

--- web/index.php
+++ web/index.php
@@ -3,10 +3,17 @@
 declare(strict_types=1);
 
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+// defined('YII_DEBUG') or define('YII_DEBUG', true);
+// defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require __DIR__ . '/../vendor/autoload.php';
+// Muat environment variables dari .env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+// Definisikan konstanta Yii
+defined('YII_DEBUG') or define('YII_DEBUG', isset($_ENV['YII_DEBUG']) && $_ENV['YII_DEBUG'] === 'true');
+defined('YII_ENV') or define('YII_ENV', $_ENV['YII_ENV'] ?? 'prod');
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
