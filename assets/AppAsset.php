174db2f538269deb73acd0d6b95244bed664bced
Rudi Gigih Prabowo <rudigigih0719@gmail.com>
2026-08-26 16:53:28 +0700

    feat: menambahkan link cdn bootstrap icons

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -27,6 +27,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css',
     ];
     public $js = [
         'js/color-mode.js',
