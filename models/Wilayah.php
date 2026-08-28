f5a9eaddd0098884b43890feb0746216d1185196
Rudi Gigih Prabowo <rudigigih0719@gmail.com>
2026-08-28 16:34:41 +0700

    feat: tambah method static untuk telusuri hirarki wilayah ke atas

--- models/Wilayah.php
+++ models/Wilayah.php
@@ -73,4 +73,49 @@ class Wilayah extends \yii\db\ActiveRecord
             ->indexBy('kode')
             ->column();
     }
+
+    /**
+     * Mengambil detail nama Provinsi, Kabupaten, Kecamatan, Kelurahan berdasarkan kode kelurahan (kode_adm4)
+     * 
+     * @param string $kodeKelurahan (Contoh: '31.72.01.1001' atau '3172011001')
+     * @return array
+     */
+    public static function getDetailWilayahByKode($kodeKelurahan)
+    {
+        if (empty($kodeKelurahan)) {
+            return [
+                'provinsi'  => '-',
+                'kabupaten' => '-',
+                'kecamatan' => '-',
+                'kelurahan' => '-',
+            ];
+        }
+
+        $kelurahan = static::findOne(['kode' => $kodeKelurahan]);
+        if (!$kelurahan) {
+            return [
+                'provinsi'  => '-',
+                'kabupaten' => '-',
+                'kecamatan' => '-',
+                'kelurahan' => '-',
+            ];
+        }
+
+        // Ambil data induk (Kecamatan, Kabupaten, Provinsi) berdasarkan relasi parent atau potongan kode
+        // Contoh jika menggunakan potongan kode standar BPS/Kemendagri (misal: 31.72.01.1001):
+        $kodeKecamatan = substr($kodeKelurahan, 0, 8); // 31.72.01
+        $kodeKabupaten = substr($kodeKelurahan, 0, 5); // 31.72
+        $kodeProvinsi  = substr($kodeKelurahan, 0, 2); // 31
+
+        $kecamatan = static::findOne(['kode' => $kodeKecamatan]);
+        $kabupaten = static::findOne(['kode' => $kodeKabupaten]);
+        $provinsi  = static::findOne(['kode' => $kodeProvinsi]);
+
+        return [
+            'provinsi'  => $provinsi->nama ?? '-',
+            'kabupaten' => $kabupaten->nama ?? '-',
+            'kecamatan' => $kecamatan->nama ?? '-',
+            'kelurahan' => $kelurahan->nama ?? '-',
+        ];
+    }
 }
