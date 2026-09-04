838a4a8032f806184586378e79de61f66d7ddc6d
Rudi Gigih Prabowo <rudigigih0719@gmail.com>
2026-09-04 11:04:24 +0700

    feat(config): use APP_NAME env variable for app name

--- config/web.php
+++ config/web.php
@@ -6,6 +6,7 @@ $db = require __DIR__ . '/db.php';
 $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
+    'name' => strtoupper(str_replace('-', ' ', $_ENV['APP_NAME'] ?? Yii::$app->name)),
     'bootstrap' => ['log'],
     'container' => [
         'singletons' => [
